450f52c77f9d871f583253e88a680a8d38f8ec78
useEffects <joel.sr1024@gmail.com>
2023-10-31 08:54:20 +0530

    push

---
+++ story.py
@@ -0,0 +1,7 @@
+chapter1 = "In the quiet town of a mysterious villain known as the Shadowheart began terrorizing the residents."
+chapter2 = "Sarah with her keen intuition and Alice an eccentric inventor joined forces to stop the Shadowheart."
+chapter3 = "As they investigated they discovered a pattern to the Shadowheart crimes leading them to believe there was a hidden motive."
+chapter4 = "The villain true identity was unveiled: Kate who had been embezzling town funds and needed to divert attention from his crimes."
+chapter5 = "With clever traps and teamwork Sarah and Alice confronted Kate bringing justice back to and ensuring the town's peace."
+
+story = [chapter1, chapter2, chapter3, chapter4, chapter5]
